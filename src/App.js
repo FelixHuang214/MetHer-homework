14a4ed8648867b8a1906faea5a2894902736ff8a
Eshau <david021484@gmail.com>
2021-12-30 13:37:43 +0800

    Add VerifyPage.js & background image

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import styled from "styled-components";
+
 import Form from "./components/Form";
 import LoginPage from "./pages/LoginPage";
 import SendEmailPage from "./pages/SendEmailPage";
 import SignupPage from "./pages/SignupPage";
+import VerifyPage from "./pages/VerifyPage";
+
+import bg from "./images/bg.png";
+
+const Container = styled.div`
+  position: relative;
+  width: 100%;
+  height: 100vh;
+`;
+
+const Bg = styled.img`
+  position: absolute;
+  width: 100%;
+  height: 100%;
+`;
+
 function App() {
-  return <LoginPage />;
+  return (
+    <Container>
+      <Bg src={bg} />
+      <VerifyPage />
+    </Container>
+  );
 }
 
 export default App;
